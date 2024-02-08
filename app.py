dc8e141bd39b9bdf407893ac4895cd66335db4bb
Rajobrata Dasgupta <dasguptarajobrata@gmail.com>
2024-02-08 14:48:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask,render_template,request,redirect,url_for
+
+## create the flask app
+
+app=Flask(__name__)
+
+@app.route('/',methods = ['GET'])
+def home():
+    return "<h2>Hello, World!</h2>"
+
+@app.route("/test2")
+def test2():
+    data = request.args.get('x')
+    return  "this is a data input form my url {}".format(data)
+#(during execution time ?x=)
+@app.route('/welcome',methods = ['GET'])
+def welcome():
+    return "<h1>Welcome to the Flask Tutorials</h1>"
+
+@app.route('/index',methods = ['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "the person is passed and the score is "+str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "the person has failed and the score is "+str(score)
+@app.route('/form',methods=['POST','GET'])
+def form():
+    if request.method=='GET':
+        return render_template('form.html')
+    
+@app.route('/calculate',methods=['POST','GET'])
+def calculate():
+    if request.method=='GET':
+        return render_template('calculate.html')
+    else:
+        maths=float(request.form['maths'])
+        science=float(request.form['science'])
+        history=float(request.form['history'])
+
+        average_marks=(maths+science+history)/3
+        result="" 
+        if average_marks>=50:
+            result="success"
+        else:
+            result="fail"
+
+        #return redirect(url_for(result,score=average_marks))
+
+
+        return render_template('result.html',results=average_marks)
+if __name__=='__main__':
+    app.run(debug=True)
